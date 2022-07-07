475d05ef41627861ac9aede7ab291f8e2d0af639
tblaase <tblaase@student.42heilbronn.de>
2022-07-07 19:24:33 +0200

    terror client to DDOS the server, but without requests

---
+++ init/client/src/main.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+int main()
+{
+	for (int i = 0; i < 2000; ++i)
+	{
+		// system("nc localhost 8888 &");
+		pid_t pid = fork();
+		if (pid == 0)
+		{
+			while (true)
+			{
+				execlp("nc", "nc", "localhost", "8080", NULL);
+				exit(-1);
+			}
+		}
+		else if (pid == -1)
+		{
+			dprintf(2, "fork %d failed\n", i);
+		}
+		else
+			printf("child %d\n", i);
+	}
+	wait(NULL);
+	return (0);
+}

3ebd047d38068aa64e6bd6cd46daa036109b06de
tblaase <tblaase@student.42heilbronn.de>
2022-07-14 17:33:23 +0200

    prep for paser lvl 3

--- init/server/include/SingleServer.hpp
+++ init/server/include/SingleServer.hpp
@@ -18,8 +18,11 @@ class SingleServer
 		std::string _serverName;
 		std::string _root;
 		std::string _index;
-		std::string _cgi_param;
-		std::pair<std::string, std::string> _location; // check if this is good, it is for the cgi stuff
+		std::pair<std::string, std::string> *_location;
+
+	// Private Methods
+		void setVariables(std::string server);
+		void checkVariables();
 
 	public:
 	// Constructors
@@ -31,8 +34,35 @@ class SingleServer
 	// Public Methods
 
 	// Getter
-	const std::string *getListen() const;
-	const std::string getServerName() const;
+		const std::string *getListen() const;
+		const std::string getServerName() const;
+		const std::string getRoot() const;
+		const std::string getIndex() const;
+		const std::pair<std::string, std::string>* getLocation() const;
 
+	// Exceptions
+		class NoRootException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class NoIndexException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class NoPortException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class NotAPortException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 };
 #endif // SINGLESERVER_HPP
--- init/server/src/SingleServer.cpp
+++ init/server/src/SingleServer.cpp
@@ -1,16 +1,21 @@
 #include "SingleServer.hpp"
 
 // Constructors
+SingleServer::SingleServer()
+{
+	std::cout << "SingleServer default constructor called" << std::endl;
+}
+
 SingleServer::SingleServer(std::string server)
 {
-	std::cout << "SingleServer Default Constructor called" << std::endl;
+	std::cout << "SingleServer constructor called" << std::endl;
 	std::cout << "This content reached the server:\n>" << server << "<" << std::endl;
 }
 
 // Deconstructors
 SingleServer::~SingleServer()
 {
-	std::cout << "SingleServer Deconstructor called" << std::endl;
+	std::cout << "SingleServer deconstructor called" << std::endl;
 	/*CODE*/
 }
 

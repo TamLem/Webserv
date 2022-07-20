f33a64ae19fa01611e3d848a528f1753c1bb606d
tblaase <tblaase@student.42heilbronn.de>
2022-07-20 21:55:22 +0200

    added the configStruct and LocationStruct, as well as include guards

--- init/server/include/Base.hpp
+++ init/server/include/Base.hpp
@@ -1,3 +1,11 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <vector>
+#include <map>
+#include <string>
+// #include <iostream>
+#include <stdbool.h>
 /* put here includes and defines that are needed for the whole project only */
 
 #define RESET "\033[0m"
@@ -12,3 +20,37 @@
 #define CRLFTWO "\r\n\r\n"
 
 #define WHITESPACE "\n\r\t\f\v "
+
+// LocationStruct
+struct LocationStruct
+{
+	bool isDir;
+	std::string root;
+	std::string indexPage;
+	bool autoIndex;
+	bool getAllowed;
+	bool postAllowed;
+	bool deleteAllowed;
+};
+
+// ConfigStruct
+struct ConfigStruct
+{
+	std::string								serverName;
+
+	std::vector<std::string>				listen;
+	std::string								root;
+	bool									autoIndex;
+	std::string								indexPage;
+	bool									chunkedTransfer;
+	size_t									clientBodyBufferSize;
+	size_t									clientMaxBodySize;
+	std::vector<std::string>				cgi;
+	std::string								cgiBin;
+	std::map<std::string, LocationStruct>	location;
+	std::vector<std::string>				errorPage;
+	bool									showLog;
+};
+
+
+#endif // BASE_HPP

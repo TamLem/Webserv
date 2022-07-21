82c36989654191e3ee112d20049b8eb26c92fee2
tblaase <tblaase@student.42heilbronn.de>
2022-07-21 19:18:18 +0200

    removed define guards

--- init/server/include/Base.hpp
+++ init/server/include/Base.hpp
@@ -9,51 +9,19 @@
 
 /* put here includes and defines that are needed for the whole project only */
 // Colors and Printing
-#ifndef RESET
-	#define RESET "\033[0m"
-#endif
-
-#ifndef GREEN
-	#define GREEN "\033[32m"
-#endif
-
-#ifndef YELLOW
-	#define YELLOW "\033[33m"
-#endif
-
-#ifndef BLUE
-	#define BLUE "\033[34m"
-#endif
-
-#ifndef RED
-	#define RED "\033[31m"
-#endif
-
-#ifndef BOLD
-	#define BOLD "\033[1m"
-#endif
-
-#ifndef UNDERLINED
-	#define UNDERLINED "\033[4m"
-#endif
-
+#define RESET "\033[0m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define RED "\033[31m"
+#define BOLD "\033[1m"
+#define UNDERLINED "\033[4m"
 
 // other defines
-#ifndef CRLF
-	#define CRLF "\r\n"
-#endif
-
-#ifndef CRLFTWO
-	#define CRLFTWO "\r\n\r\n"
-#endif
-
-#ifndef WHITESPACE
-	#define WHITESPACE "\n\r\t\f\v "
-#endif
-
-#ifndef DECIMAL
-	#define DECIMAL "0123456789"
-#endif
+#define CRLF "\r\n"
+#define CRLFTWO "\r\n\r\n"
+#define WHITESPACE "\n\r\t\f\v "
+#define DECIMAL "0123456789"
 
 // LocationStruct
 struct LocationStruct
@@ -71,19 +39,19 @@ struct LocationStruct
 struct ConfigStruct
 {
 	std::string								serverName;
-
 	std::vector<std::string>				listen;
 	std::string								root;
-	bool									autoIndex;
-	std::string								indexPage;
-	bool									chunkedTransfer;
-	size_t									clientBodyBufferSize;
-	size_t									clientMaxBodySize;
 	std::vector<std::string>				cgi;
 	std::string								cgiBin;
+	size_t									clientBodyBufferSize;
+
+	size_t									clientMaxBodySize;
+	std::string								indexPage;
 	std::map<std::string, LocationStruct>	location;
 	std::vector<std::string>				errorPage;
+	bool									autoIndex;
 	bool									showLog;
+	bool									chunkedTransfer;
 };
 
 

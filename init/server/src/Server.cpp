b6ee2ce26a07e429a42891b94b3e176f06bf8520
Andreas Englert <aenglert@3-C-7.42heilbronn.de>
2022-07-22 11:02:44 +0200

    moved Server member function definitions from hpp to cpp file

--- init/server/src/Server.cpp
+++ init/server/src/Server.cpp
@@ -1,6 +1,183 @@
 #include "Server.hpp"
 #include "Config.hpp"
 
+Server::Server(void)
+{
+	std::cout << "Server default constructor called for " << this << std::endl;
+}
+
+Server::Server(int port)
+{
+	std::cout << "Server constructor called for " << this << std::endl;
+	_port = port;
+
+	if ((_server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		std::cerr << RED << "Error creating socket" << std::endl;
+		perror(NULL);
+		std::cerr << RESET;
+		return;
+	}
+
+// Set socket reusable from Time-Wait state
+	int val = 1;
+	setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &val, 4); // how to implement | SO_NOSIGPIPE
+
+// initialize server address struct
+	struct sockaddr_in serv_addr;
+	memset(&serv_addr, '0', sizeof(serv_addr)); // is memset allowed? !!!!!!!!!!!!!!!!!!!!
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+// bind socket to address
+	if((bind(_server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr))) < 0)
+	{
+		std::cerr << RED << "Error binding socket" << std::endl;
+		perror(NULL);
+		std::cerr << RESET;
+		exit(EXIT_FAILURE);
+	}
+}
+
+Server::~Server(void)
+{
+	close(this->_server_fd);
+	std::cout << "server deconstructor called for " << this << std::endl;
+}
+
+void Server::stop(void){}
+
+int Server::get_client(int fd)
+{
+	for (size_t i = 0; i < _clients.size(); i++)
+	{
+		if (_clients[i].fd == fd)
+			return (i);
+	}
+	return (-1);
+}
+
+int Server::add_client(int fd, struct sockaddr_in addr)
+{
+	struct client c;
+	c.fd = fd;
+	c.addr = addr;
+	_clients.push_back(c);
+	return (_clients.size() - 1);
+}
+
+int Server::remove_client(int fd)
+{
+	int i = get_client(fd);
+	if (i == -1)
+		return (-1);
+	_clients.erase(_clients.begin() + i);
+	return (0);
+}
+
+void Server::run_event_loop(int kq)
+{
+	struct kevent ev;
+	struct kevent evList[MAX_EVENTS];
+	struct sockaddr_storage addr;
+
+	while(keep_running)
+	{
+		int num_events = kevent(kq, NULL, 0, evList, MAX_EVENTS, NULL);
+		for (int i = 0; i < num_events; i++)
+		{
+			if (evList[i].ident == _server_fd)
+			{
+				socklen_t addrlen = sizeof(addr);
+				int fd = accept(_server_fd, (struct sockaddr *)&addr, &addrlen);
+				if (fd < 0)
+				{
+					std::cerr << RED << "Error accepting connection" << std::endl;
+					perror(NULL);
+					std::cerr << RESET;
+					continue;
+				}
+				else
+					std::cout << GREEN << "New connection on socket " << fd << RESET << std::endl;
+				int set = 1;
+				setsockopt(fd, SOL_SOCKET, SO_NOSIGPIPE, (void *)&set, sizeof(int)); // set socket to not SIGPIPE
+				add_client(fd, *(struct sockaddr_in *)&addr);
+				EV_SET(&ev, fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+				kevent(kq, &ev, 1, NULL, 0, NULL);
+			}
+			else if (evList[i].flags & EV_EOF) //handle client disconnect event
+			{
+				remove_client(evList[i].ident);
+				EV_SET(&ev, evList[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+				kevent(kq, &ev, 1, NULL, 0, NULL);
+				std::cout << GREEN << "Client " << evList[i].ident << " disconnected" << RESET << std::endl;
+			}
+			else if (evList[i].flags & EVFILT_READ) //handle client read event
+			{
+				int fd = evList[i].ident;
+				int i = get_client(fd);
+				if (i == -1)
+				{
+					std::cerr << RED << "Error getting client" << std::endl;
+					perror(NULL);
+					std::cerr << RESET;
+					continue;
+				}
+				char buf[1024]; // probably needs to be an ifstream to not overflow with enormous requests
+				int n = read(fd, buf, 1024);
+				if (n < 0)
+				{
+					std::cerr << RED << "Error reading from client" << std::endl;
+					perror(NULL);
+					std::cerr << RESET;
+					continue;
+				}
+				buf[n] = '\0';
+				std::cout << YELLOW << "Received->" << RESET << buf << YELLOW << "<-Received" << RESET << std::endl;
+
+				if (std::string(buf).find(".php") != std::string::npos)
+					cgi_response(buf, fd);
+				else
+					handle_static_request(buf, fd);
+			}
+		}
+	}
+}
+
+void Server::run()
+{
+	if (listen(_server_fd, 5))
+	{
+		std::cerr << RED << "Error listening" << std::endl;
+		perror(NULL);
+		std::cerr << RESET;
+		return;
+	}
+	else
+		std::cout << "Listening on port " << _port << std::endl;
+// create a kqueue
+	int kq = kqueue();
+	if (kq == -1)
+	{
+		std::cerr << RED << "Error creating kqueue" << std::endl;
+		perror(NULL);
+		std::cerr << RESET;
+		return;
+	}
+	struct kevent ev;
+	EV_SET(&ev, _server_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+	int ret = kevent(kq, &ev, 1, NULL, 0, NULL);
+	if (ret == -1)
+	{
+		std::cerr << RED << "Error adding server socket to kqueue" << std::endl;
+		perror(NULL);
+		std::cerr << RESET;
+		return;
+	}
+	run_event_loop(kq);
+}
+
 void Server::handleGET(int status, int fd, const std::string& uri)
 {
 	Response.init(status, fd, uri);
---
+++ init/server/src/main.cpp
@@ -0,0 +1,81 @@
+#include "Server.hpp"
+#include "Config.hpp"
+
+static void handle_signal(int sig)
+{
+	if (sig == SIGINT)
+	{
+		std::cerr << BLUE << "SIGINT detected, terminating server now" << RESET << std::endl;
+		keep_running = 0;
+	}
+	// else if (sig == SIGPIPE)
+	// {
+	// 	std::cerr << RED << "SIGPIPE detected, will crash now" << RESET << std::endl;
+	// 	keep_running = 0;
+	// }
+}
+
+// check if signal is forbidden!!!!!!!!!!!!!!!!!
+void	handle_signals(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, SIG_IGN);
+	// signal(SIGPIPE, SIG_IGN);
+	signal(SIGINT, handle_signal);
+	// signal(SIGPIPE, handle_signal);
+}
+
+void parseArgv(int argc, char **argv) // maybe change to static void function or include it into some object
+{
+	if (argc <= 1 || argc > 2)
+	{
+		std::cerr << RED << "Please only use webserv with config file as follows:" << std::endl << BLUE << "./webserv <config_filename>.conf" << RESET << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::string sArgv = argv[1];
+	std::string ending = ".conf";
+	if ((argv[1] + sArgv.find_last_of(".")) != ending)
+	{
+		std::cerr << RED << "Please only use webserv with config file as follows:" << std::endl << BLUE << "./webserv <config_filename>.conf" << RESET << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	handle_signals();
+	parseArgv(argc, argv);
+	Config *config = new Config();
+	try
+	{
+		config->start(argv[1]);
+	}
+	catch (std::exception &e)
+	{
+			std::cerr << RED << "Exception caught in main function: " << e.what() << RESET << std::endl;
+			delete config;
+			return (EXIT_FAILURE);
+	}
+	Server *test = new Server(8080/*, &config*/); // somehow pass the listen ports to the server ??
+	// std::cout << BLUE << test->cluster.size() << " elements found inside map" << RESET << std::endl;
+	// test if the data inside the cluster is accessable
+	std::string firstName = "weebserv";
+	std::string secondName = "anotherone";
+
+	// SingleServerConfig first = test->cluster[firstName];
+	// SingleServerConfig second = test->cluster[secondName];
+	std::cout << "### attempting to print contents of the configStructs" << std::endl;
+	config->applyConfig(firstName);
+	std::cout << config << std::endl;
+	config->applyConfig(secondName);
+	std::cout << config << std::endl;
+	// std::cout << RED << first.getServerName() << "<->" << second.getServerName() << RESET << std::endl;
+	// system("leaks webserv");
+	test->run();
+	delete config;
+	delete test;
+	config = NULL;
+	test = NULL;
+	// system("leaks webserv");
+	return (0);
+}

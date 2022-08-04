64f46160152511faf14939d60b714bb199b76b16
Andreas Englert <aenglert@1-C-32.42heilbronn.de>
2022-08-04 17:09:21 +0200

    moved utf-8 decoding tag from header fields to html head

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -107,21 +107,21 @@ void Server::handleGET(const Request& request)
 		|| this->_currentConfig.autoIndex == true)
 		{
 			_response.createIndex(request.getTarget());
-			_response.addHeaderField("Content-Type", "text/html; charset=utf-8");
+			// _response.addHeaderField("Content-Type", "text/html; charset=utf-8");
 		}
 		else
 		{
 			// std::cerr << BOLD << RED << "target1:" << request.getTarget() << RESET << std::endl;
 			// std::cerr << BOLD << RED << "indexPage:" << request.indexPage << RESET << std::endl;
 			_response.createBodyFromFile(request.getTarget() + request.indexPage);
-			_response.addHeaderField("Content-Type", "text/html; charset=utf-8");
+			// _response.addHeaderField("Content-Type", "text/html; charset=utf-8");
 		}
 	}
 	else
 	{
 			// std::cerr << BOLD << RED << "target2:" << request.getTarget() << RESET << std::endl;
 		_response.createBodyFromFile(request.getTarget() + request.indexPage);
-		_response.addHeaderField("Content-Type", "text/html; charset=utf-8");
+		// _response.addHeaderField("Content-Type", "text/html; charset=utf-8");
 	}
 	_response.addHeaderField("Server", this->_currentConfig.serverName);
 	_response.addDefaultHeaderFields();

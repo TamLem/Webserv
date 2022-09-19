b6eb9141b086e7ee1095420cb8b75fe1f610cdb9
Andreas Englert <aenglert@2-I-1.42heilbronn.de>
2022-09-20 00:22:57 +0200

    XXX comments on cgi bugs

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -460,11 +460,11 @@ void Server::checkLocationMethod(const Request& request) const
 bool Server::_isCgiRequest(std::string requestHead) // AE this function ahs to be included in locationMatching
 {
 	requestHead = requestHead.substr(0, requestHead.find("HTTP/1.1")); // AE is formatting checked before?
-	// if (requestHead.find("/cgi/") != std::string::npos) // AE file extension should deterime if something is cgi or not
-	// 	return (true);
-	/*if (this->_currentConfig.cgiBin.length() != 0 && requestHead.find(this->_currentConfig.cgiBin) != std::string::npos)
+	if (requestHead.find("/cgi/") != std::string::npos) // AE file extension should deterime if something is cgi or not
 		return (true);
-	else */if (this->_currentConfig.cgi.size() != 0)
+	if (this->_currentConfig.cgiBin.length() != 0 && requestHead.find(this->_currentConfig.cgiBin) != std::string::npos)
+		return (true);
+	else if (this->_currentConfig.cgi.size() != 0)
 	{
 		std::map<std::string, std::string>::const_iterator it = this->_currentConfig.cgi.begin();
 		for (; it != this->_currentConfig.cgi.end(); ++it)

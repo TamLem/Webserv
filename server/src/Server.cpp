40ef056ee2162c0f8faa6cdf9dfda3da0f3c79bc
Andreas Englert <aenglert@2-I-5.42heilbronn.de>
2022-09-20 18:51:39 +0200

    raised buffer size for PUT

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -203,7 +203,7 @@ void Server::handlePOST(int clientFd, const Request& request)
 		// this->_response.addHeaderField("connection", "close");
 		this->_response.setStatus("201");
 		this->_response.setPostTarget(clientFd, request.getRoutedTarget()); // puts target into the response class
-		this->_response.setPostBufferSize(clientFd, this->_currentConfig.clientBodyBufferSize);
+		this->_response.setPostBufferSize(clientFd, 100000);
 		this->_response.setPostChunked(clientFd, request.getRoutedTarget(), tempHeaderFields);
 		return ;
 	}
